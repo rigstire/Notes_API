ecff8f6d2fce4cd87b013a78ba54b4ae468af4df
rigstire <brodsky.nick@gmail.com>
2021-06-01 13:12:45 -0700

    your message goes here

--- posts/models.py
+++ posts/models.py
@@ -4,10 +4,9 @@ from django.urls import reverse
 class Notes(models.Model):
     title = models.CharField(max_length=300)
     body = models.TextField()
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.title[:50]
 
     def get_absolute_url(self):
-        return reverse('post_detail',args=[str(self.id)])
+        return reverse('home')
